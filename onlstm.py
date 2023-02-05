22f2644fe64a8431dbb9cb3aadb30428716a6249
kulkarniakash <58792926+kulkarniakash@users.noreply.github.com>
2023-02-04 22:17:34 -0600

    Deleted old commented out code lines

--- onlstm.py
+++ onlstm.py
@@ -141,17 +141,8 @@ class ModelHandler:
         self.model.add(layers.Dense(vocab_size+1, activation="softmax", name="output"))
         self.x_train = x_train.reshape((-1, self.sequence_len)).astype('int32')
         
-        # # take in [batch_size, input_length] where input_length is the length of a sequence of 
-        # # inputs
-        # embed = layers.Embedding(vocab_size+1, self.emb_dim, name="embed")
-        # # outputs shape of (batch_size, input_length, output_dim)
-        # v = embed(inputs)
-        # #[batch_size, time_steps, input_dim]
-        # onlstm_outputs = layers.RNN(ONLSTM_Cell(self.h_units),name="onlstm")(v)
-        # output = layers.Dense(vocab_size+1, activation="softmax", name="output")(onlstm_outputs)
-        # self.model = keras.Model(inputs=inputs, outputs=output)
+
         self.model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.SparseCategoricalCrossentropy())
-        # print_weights = LambdaCallback(on_epoch_end=lambda batch, logs: print(model.layers[0].get_weights()))
         self.model.fit(x_train, y_train, epochs=epochs, callbacks=[perp_callback], verbose=2)
         
     def buildModelLSTM(self, max_sentences, epochs):
@@ -171,26 +162,6 @@ class ModelHandler:
         self.model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.SparseCategoricalCrossentropy())
         self.model.fit(x_train, y_train, epochs=epochs, callbacks=[perp_callback], verbose=2)
         
-# max_sentences = 100
-# # words = read_file(filename)
-# words = read_lines(filename)
-# words = words[:max_sentences]
-# words = flatten_sentences(words)
-# # dictionary, reverse_dict = build_dictionary(words)
-# dictionary, reverse_dict = build_dictionary(words)
-# doc_size = len(words)
-# vocab_size = len(dictionary) 
-# n_hidden = 512
-# emb_dim = 10
-
-# rnn_units = n_hidden
-# input_dim = emb_dim
-# batch_size = 1
-# # time_step = 3
-# sequence_len = 20
-
-
-
 
 
 class GateLayer(layers.Layer):
@@ -210,7 +181,6 @@ class GateLayer(layers.Layer):
                                               trainable=True)
         self.bias = self.add_weight(shape=(self.units,), initializer="random_normal", 
                                     trainable=True)
-        # self.built = True
         
     @tf.function
     def call(self, inputs):
@@ -228,21 +198,8 @@ class ONLSTM_Cell(layers.Layer):
         self.units = units
         self.state_size = [units, units]
         self.output_size = units
-        # sig = tf.nn.sigmoid
-        # tanh = tf.nn.tanh 
-        
-        # self.f_gate = tf.function(GateLayer(sig, self.units))
-        # self.i_gate = tf.function(GateLayer(sig, self.units))
-        # self.o_gate = tf.function(GateLayer(sig, self.units))
-        # self.c_hat_gate = tf.function(GateLayer(tanh, self.units))
-        
-        # self.f_tilde = tf.function(GateLayer(cumax, self.units))
-        # self.i_tilde_mod = tf.function(GateLayer(cumax, self.units))
+
         super().__init__()
-        
-    # @property
-    # def state_size(self):
-    #   return [self.units, self.units]
   
 
     def build_gate_weights(self, input_shape, activation_func):
@@ -258,7 +215,7 @@ class ONLSTM_Cell(layers.Layer):
          return input_weights, hidden_weights, bias, activation_func
     
 
-    # inputs.shape = (None, units + emb_dim)
+    
     def call_gate(self, gate, inputs):
         input_weights, hidden_weights, bias, activation_func = gate
         x_t = inputs[:, :(inputs.shape[-1] - self.units)]
@@ -282,18 +239,8 @@ class ONLSTM_Cell(layers.Layer):
         self.built = True
         
     
-    # def get_initial_state():
-    #     return tf.constant([tf.zeros([batch_size, rnn_units]), tf.zeros([batch_size, rnn_units])])
-    
-    # inputs.shape = (None, emb_dim)
     def call(self, inputs, states):
         tanh = tf.nn.tanh
-        
-        # # is it processing all the batches though?
-        # inputs = tf.reshape(inputs, (-1,))
-        # print("Input Shape", inputs[2])
-        # print("State Shape", tf.shape(states[0]))
-        # inputs = tf.reshape(inputs, (-1, inputs.shape[-1]))
         
         if states == None:
             states = [tf.zeros(self.units), tf.zeros(self.units)]
@@ -319,9 +266,6 @@ class ONLSTM_Cell(layers.Layer):
         
         return hidden, [hidden, cell]
 
-# assumes batch size is 1!!
-# @tf.function(input_signature=(tf.TensorSpec(shape=[None], dtype=tf.float32),), 
-#               experimental_relax_shapes=True)
 def cumax(array):
     return tf.cumsum(tf.nn.softmax(array))
 
@@ -383,54 +327,3 @@ ph_on = Perplexity(mh_on)
 perp_on, known_ratio_on = ph_on.calculate_bigram_perp(dh_test, max_sentences_test)
 
 
-# t = tf.constant([1,2.,3]) 
-# t =  tf.reshape(t, [batch_size, sequence_len])
-# t = t.numpy()
-# rng = default_rng()
-# x_train, y_train = build_dataset(words)
-# # [samples, time_step, feature]
-# # x_train = tf.keras.preprocessing.sequence.pad_sequences(x_train, padding='post', dtype='int32')
-# x_train = x_train.reshape((-1, sequence_len)).astype('int32')
-# y_train = y_train.reshape((-1, y_train.shape[0])).T
-
-# y_train = y_train.reshape(-1, y_train.shape[0])
-# x_train = x_train.reshape(-1, sequence_len, 1).astype('float32')
-
-# rng = default_rng()
-# # initial_state =  [tf.zeros([batch_size, rnn_units]), tf.zeros([batch_size, rnn_units])]
-# model = None
-
-# def fit_model():
-#     states = [tf.constant(rng.uniform(-1, 1, rnn_units)), tf.constant(rng.uniform(-1, 1, rnn_units))]
-#     inputs = keras.Input((sequence_len))
-#     # take in [batch_size, input_length] where input_length is the length of a sequence of 
-#     # inputs
-#     embed = layers.Embedding(vocab_size+1, input_dim, name="embed")
-#     # outputs shape of (batch_size, input_length, output_dim)
-#     v = embed(inputs)
-#     #[batch_size, time_steps, input_dim]
-#     onlstm_outputs = layers.RNN(ONLSTM_Cell(rnn_units),name="onlstm")(v)
-#     output = layers.Dense(vocab_size+1, activation="softmax", name="output")(onlstm_outputs)
-#     model = keras.Model(inputs=inputs, outputs=output)
-#     model.compile(optimizer=keras.optimizers.Adam(), loss=keras.losses.SparseCategoricalCrossentropy())
-#     # print_weights = LambdaCallback(on_epoch_end=lambda batch, logs: print(model.layers[0].get_weights()))
-#     model.fit(x_train, y_train, epochs=200, verbose=2)
-    
-# fit_model()
-
-# def predict_next(input_words):
-#     input_words = np.array([dictionary[word] for word in input_words])
-#     input_words = input_words.reshape(-1, len(input_words), 1)
-#     res = np.array(model.predict(input_words))
-#     res = res.reshape(vocab_size)
-#     return reverse_dict[tf.math.argmax(res).numpy()]
-
-# print(predict_next(["the", "mice", "had", "a", "general", "council", "to"]))
-
-# print(model.predict_class(t))
-
-# n_gram = keras.Input(shape=(n_input, ))
-# word_emb = layers.Embedding(vocab_size, emb_dim)(n_gram)
-# output_gate = layer.Dense()
-# y = np.array([1,2,3]).reshape(-1, 1, 3).astype('float32')
-# _, state_h, state_c = layers.LSTM(10, return_state=True)(y)
